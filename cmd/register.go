15f18205c1853fc98e2f70b2f44a55a1c049f856
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2018-10-23 15:23:20 +0800

    Update cmd/register.go

--- cmd/register.go
+++ cmd/register.go
@@ -31,7 +31,6 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("register called")
 		username, _ := cmd.Flags().GetString("user")
 		fmt.Println("register called by " + username)
 	},
