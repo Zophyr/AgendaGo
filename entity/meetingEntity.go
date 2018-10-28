297843f44d8f4289bbaf3c4416b5728c157b8bd7
hongzicong <973138379@qq.com>
2018-10-29 00:36:41 +0800

    update code location

--- entity/meetingEntity.go
+++ entity/meetingEntity.go
@@ -1,5 +1,10 @@
 package entity
 
+import (
+	"fmt"
+	"os"
+)
+
 type Meeting struct {
 	Title         string   `json:"tile"`
 	Sponsor       string   `json:"sponsor"`
@@ -51,4 +56,38 @@ func (model *meetingModel) DeleteParticipatorFromMeeting(meeting *Meeting, parti
 func (model *meetingModel) AddParticipatorToMeeting(meeting *Meeting, participator string) {
 	curMeetingParticipators := model.meetings[meeting.Title].Participators
 	model.meetings[meeting.Title].Participators = append(curMeetingParticipators, participator)
+}
+
+func (model *meetingModel) AddMeeting(meeting *Meeting) {
+	_, ok := model.meetings[meeting.Title]
+	if ok == false {
+		model.meetings[meeting.Title] = meeting
+		fmt.Printf("adding meeting : %s\n", meeting.Title)
+	} else {
+		fmt.Println(os.Stderr, "Error:%s", "already exists a meeting with the same title")
+	}
+}
+
+func (model *meetingModel) queryMeeting(title string) (*Meeting, bool) {
+	_, ok := model.meetings[title]
+	if ok == true {
+		return model.meetings[title], true
+	} else {
+		fmt.Println(os.Stderr, "Error:%s", "no such meeting")
+		return nil, false
+	}
+}
+
+func (meeting *Meeting) getParticipator() []string {
+	return meeting.Participators
+}
+
+func (model *meetingModel) deleteMeeting(title string) bool {
+	if _, ok := model.meetings[title]; ok {
+		delete(model.meetings, title)
+		return true
+	} else {
+		fmt.Println(os.Stderr, "error:%s\n", "no such meeting to delete")
+		return false
+	}
 }
