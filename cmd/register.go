dbdec59dc5a0cccffd7a0a090b15557e42a95b3d
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2018-10-30 14:25:27 +0800

    Update cmd/register.go

--- cmd/register.go
+++ cmd/register.go
@@ -1,16 +1,4 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright © 2018 Zophyr <the-zephyr@hotmail.com>
 
 package cmd
 
@@ -32,22 +20,15 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		username, _ := cmd.Flags().GetString("username")
-		fmt.Println("register called by " + username)
+		password, _ := cmd.Flags().GetString("password")
+		email, _ := cmd.Flags().GetString("email")
+		phone, _ := cmd.Flags().GetString("phone")
+		fmt.Println("register called by " + username + " password " + password + " email " + email + " phone " + phone)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(registerCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// registerCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// registerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	
 	registerCmd.Flags().StringP("username", "u", "", "用户名")
 	registerCmd.Flags().StringP("password", "p", "", "密码")
