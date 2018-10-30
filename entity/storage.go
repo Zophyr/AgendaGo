4f08cb93a492c646ac608da8244788cfb32a9c8b
hongzicong <973138379@qq.com>
2018-10-30 18:04:35 +0800

    Update entity/storage.go

--- entity/storage.go
+++ entity/storage.go
@@ -4,23 +4,22 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"utils"
 )
 
 type storage struct {
-	path string 
+	path string
 }
 
 func (s *storage) load(ptr interface{}) {
 	file, err := os.Open(s.path)
 	defer file.Close()
-	if err != nil{
-		fmt.Println(os.Stderr,"err:%s",err)
-		return		
+	if err != nil {
+		fmt.Println(os.Stderr, "err:%s", err)
+		return
 	}
 	err := json.NewDecoder(file).Decode(ptr)
-	if err!=nil{
-		fmt.Println(os.Stderr,"err:%s",err)
+	if err != nil {
+		fmt.Println(os.Stderr, "err:%s", err)
 		return
 	}
 }
@@ -29,12 +28,12 @@ func (s *storage) dump(ptr interface{}) {
 	file, err := os.OpenFile(s.path, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0666)
 	defer file.Close()
 	if err != nil {
-		fmt.Println(os.Stderr,"err:%s",err)
+		fmt.Println(os.Stderr, "err:%s", err)
 		return
 	}
 	err := json.NewEncoder(file).Encode(ptr)
 	if err != nil {
-		fmt.Println(os.Stderr,"err:%s",err)
+		fmt.Println(os.Stderr, "err:%s", err)
 		return
 	}
 }
